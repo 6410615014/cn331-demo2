4015603fe8e1ad5a25a122b25752086ebf53c6d9
aom <64106150147@student.tu.ac.th>
2025-09-08 11:17:55 +0700

    edit for superuser

--- build.sh
+++ build.sh
@@ -6,7 +6,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser \
-  --username "$DJANGO_SUPERUSER_USERNAME" \
-  --email "$DJANGO_SUPERUSER_EMAIL" \
-  --noinput || true
+python create_superuser.py
