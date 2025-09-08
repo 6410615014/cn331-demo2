c90401449a00570179dd7d78c2564a1fa761203e
aom <64106150147@student.tu.ac.th>
2025-09-08 11:23:54 +0700

    edit

--- create_superuser.py
+++ create_superuser.py
@@ -2,14 +2,14 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Airline.settings")  # เปลี่ยนถ้าโปรเจกต์ชื่อไม่ใช่ Airline
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Airline.settings")
 django.setup()
 
 User = get_user_model()
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "adminpass123")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
 
 if not User.objects.filter(username=username).exists():
     print(f"Creating superuser '{username}'...")
